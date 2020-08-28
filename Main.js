130c5c927c81d57ec1419e6ed9dda1f809e295e7
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-28 17:41:11 +0500

    Basic files and folders restructured

--- Main.js
+++ Main.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './Screens/Home';
 
 export default function Main() {
-    return(
-        <View>
+    const Stack = createStackNavigator();
 
-        </View>
+    const HomeStackNavigator = () => {
+        return(
+            <Stack.Navigator initialRouteName='Home' screenOptions={{
+                headerTitleAlign: 'center'
+            }}>
+                <Stack.Screen options={{ title: 'Foody' }} name='Home' component={Home} />
+            </Stack.Navigator>
+        )
+    }
+
+    return(
+        <NavigationContainer>
+            <HomeStackNavigator />
+        </NavigationContainer>
     );
 }
