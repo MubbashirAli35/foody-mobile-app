f912b623f4acf007c8e42f81361982cb65437953
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-29 23:26:43 +0500

    Appbar title font changed

--- Main.js
+++ Main.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Text } from 'react-native-elements';
+import { useFonts, DancingScript_700Bold } from '@expo-google-fonts/dancing-script'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './Screens/Home';
 
 export default function Main() {
+    useFonts({
+        DancingScript_700Bold
+    });
+
     const drawerNavigatorIcon = () => {
         return(
             <FontAwesome5 name='bars' solid style={{ marginLeft: 13, fontSize: 20, color: '#ed441a' }} />
@@ -15,7 +20,7 @@ export default function Main() {
 
     const appbarTitle = ( title ) => {
         return(
-            <Text h6 style={{ color: '#ed441a' }}>
+            <Text style={{ color: '#ed441a', fontFamily: 'DancingScript_700Bold', fontSize: 30 }}>
                 {title}
             </Text>
         )
